a7bba1e58c68a6e5d8d2cec2609d83391f6adcdd
willclarktech <willclarktech@users.noreply.github.com>
2019-06-24 16:08:40 +0100

    Add three-way-xor in Haskell

---
+++ tiny-toys/three-way-xor.hs
@@ -0,0 +1,27 @@
+import System.Environment (lookupEnv)
+import Utils
+    ( Network (..)
+    , generateRandomSynapses
+    , getIterations
+    , getLayerWidth
+    , sigmoid
+    , sigmoidDerivative
+    , train
+    )
+
+main = do
+    envIterations <- lookupEnv "ITERATIONS"
+    let iterations = getIterations 100000 envIterations
+
+    let x = [[0,0,0],[0,0,1],[0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,1]]
+    let y =[[0], [1], [1], [0], [1], [0], [0], [1]]
+
+    let hiddenWidth = 4
+    let widths = [getLayerWidth x, hiddenWidth, getLayerWidth y]
+    let initialSynapses = generateRandomSynapses 1337 widths
+    let initialState = Network y [x] initialSynapses
+
+    let finalState = train sigmoid sigmoidDerivative iterations initialState
+
+    print "Output after training:"
+    print $ last $ layers finalState
