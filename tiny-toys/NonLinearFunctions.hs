e4ed56019d6bef7e87f451f501459fd1159ff5dd
willclarktech <willclarktech@users.noreply.github.com>
2019-06-24 16:56:34 +0100

    Add new non-linear functions and derivatives to Haskell

--- tiny-toys/NonLinearFunctions.hs
+++ tiny-toys/NonLinearFunctions.hs
@@ -1,8 +1,14 @@
 module NonLinearFunctions
     ( NonLinearFunction
     , DerivativeFunction
+    , leakyRelu
+    , leakyReluDerivative
+    , relu
+    , reluDerivative
     , sigmoid
     , sigmoidDerivative
+    , softplus
+    , softplusDerivative
     ) where
 
 type NonLinearFunction = Float -> Float
@@ -13,3 +19,27 @@ sigmoid n = 1 / (1 + exp (-n))
 
 sigmoidDerivative :: DerivativeFunction
 sigmoidDerivative n = n * (1 - n)
+
+relu :: NonLinearFunction
+relu n = max 0 n
+
+reluDerivative :: DerivativeFunction
+reluDerivative n
+    | n <= 0 = 0
+    | otherwise = n
+
+leakyRelu :: NonLinearFunction
+leakyRelu n
+    | n <= 0 = 0.01 * n
+    | otherwise = n
+
+leakyReluDerivative :: DerivativeFunction
+leakyReluDerivative n
+    | n <= 0 = 0.01
+    | otherwise = n
+
+softplus :: NonLinearFunction
+softplus n = log (1 + exp n)
+
+softplusDerivative :: DerivativeFunction
+softplusDerivative = sigmoid
